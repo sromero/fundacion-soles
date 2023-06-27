9c36b734c00ace52a24b5cb619be1bc793ea513d
sromero <sgromero2010@gmail.com>
2023-06-27 18:16:38 -0300

    add cors

--- src/main/java/com/ues21/fundacionsoles/controller/AuthController.java
+++ src/main/java/com/ues21/fundacionsoles/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin
 public class AuthController {
 
     private AuthService authService;
