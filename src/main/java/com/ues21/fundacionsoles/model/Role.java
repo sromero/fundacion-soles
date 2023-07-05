20a26cba06fc22ae8130f85de997e86b7cd196a3
Palen17 <valentinnapo@gmail.com>
2023-07-04 22:55:27 -0300

    Correccion de Role

--- src/main/java/com/ues21/fundacionsoles/model/Role.java
+++ src/main/java/com/ues21/fundacionsoles/model/Role.java
@@ -1,10 +1,7 @@
 package com.ues21.fundacionsoles.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -17,10 +14,6 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idEstado;
+    private long id;
     private String name;
-
-    @JsonIgnoreProperties("estado")
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "area")
-    private List<Tarea> tareas;
 }
